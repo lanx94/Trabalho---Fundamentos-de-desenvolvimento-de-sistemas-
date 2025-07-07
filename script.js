401892eb5e4473cb46260ee003bd2b1dd010b55e
Lucas Cardoso da Silva <lucascardoso94@outlook.com>
2025-07-06 22:53:27 -0300

    Entraga final

---
+++ script.js
@@ -0,0 +1,6 @@
+const botao1 = document.getElementById('botao')
+const mensagem1 = document.getElementById('mensagem')
+botao1.addEventListener('click',function()
+    {
+        mensagem1.textContent = 'Palavras são, na minha nada humilde opinião, nossa inesgotável fonte de magia. Capazes de causar grandes sofrimentos e também de remediá-los';
+    })
